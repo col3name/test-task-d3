e6dbdca56b5e852c3ec4201a7c6912d137859c9e
col3name <mmikusho@instaloper.com>
2023-10-09 08:52:52 +0300

    chore: code cleanup

--- src/features/notification/slice.ts
+++ src/features/notification/slice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import {v4} from "uuid";
+import {v4 as uuidv4} from "uuid";
 
 import {Notification, NotificationId, NotificationItem, NotificationState} from './model';
 
@@ -11,7 +11,7 @@ export const notificationSlice = createSlice({
   initialState,
   reducers: {
     addNotification: (state: NotificationState, action: PayloadAction<Notification>) => {
-      state.push({ id: v4().toString(), text: action.payload.text, type: action.payload.type, timeout: TIMEOUT});
+      state.push({ id: uuidv4().toString(), ...action.payload, timeout: TIMEOUT});
       console.log(state)
     },
     removeNotification: (state: NotificationState, action: PayloadAction<NotificationId>) =>{
--- src/index.tsx
+++ src/index.tsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
-const graphQlClient = getClientWithAuth()
+const graphQlClient = getClientWithAuth();
 root.render(
   <React.StrictMode>
     <ApolloProvider client={graphQlClient}>
